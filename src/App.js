5571a75a5aad5d2d8a3df4093180ce0abd2b5836
joonas-ris <joonas.rissanen@outlook.com>
2023-02-14 13:29:08 +0200

    stuff

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
-import Login from "./Login";
-import Reserve from "./Reserve";
-import Navigation from "./Navigation";
+import Login from "./Components/Login";
+import Reserve from "./Components/Reserve";
+import Navigation from "./Components/Navigation";
+import Admin from "./Components/Admin";
 import { Container } from "react-bootstrap";
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
                 <Routes>
                     <Route exact path="/" element={<Login />} />
                     <Route path="/reserve" element={<Reserve />} />
+                    <Route path="/admin" element={<Admin />} />
                 </Routes>
             </Container>
         </Router>
---
+++ src/Components/Admin.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button, Form } from 'react-bootstrap';
+
+const Admin = () => {
+    console.log("admin");
+};
+
+
+export default Admin;
