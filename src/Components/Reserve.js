5c1596f361bdb4c0f19dafa09ff85b726677f5d9
temp-storage <pyry.vehniainen@gmail.com>
2023-02-15 10:40:10 +0200

    Added IP reservation functionality base

--- src/Components/Reserve.js
+++ src/Components/Reserve.js
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 
 const Reserve = () => {
     return (
@@ -8,5 +8,46 @@ const Reserve = () => {
         </div>
     );
 };
+
+export default Reserve; */
+
+import React, { useState, useEffect } from 'react';
+
+const Reserve = () => {
+  const [data, setData] = useState(null);
+
+  const fetchData = () => {
+    fetch('https://api.example.com/data')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error(error));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleFetchData = () => {
+    fetchData();
+  };
+
+  return (
+    <div>
+      {data === null ? (
+        <p>Sinulla ei ole aktiivisia varauksia.</p>
+      ) : (
+        <div>
+          <ul>
+            {data.map(item => (
+              <li key={item.id}>{item.text}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+      <button onClick={handleFetchData}>Varaa uusi IP-osoite</button>
+    </div>
+  );
+}
+
 
 export default Reserve;
