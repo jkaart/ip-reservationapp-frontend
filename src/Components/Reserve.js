c108799dc505d5769eac3d7c6ceaff0078869e2e
joonas-ris <joonas.rissanen@outlook.com>
2023-02-21 10:04:47 +0200

    new components implemented

--- src/Components/Reserve.js
+++ src/Components/Reserve.js
@@ -1,46 +1,33 @@
 import React, { useState, useEffect } from 'react';
+import { Container, Row, Col, Button } from "react-bootstrap";
+import UserProfile from './UserInfo';
+import IPReservation from './IPReservation';
+import IPReservedTable from './IPReservedTable';
 
 const Reserve = () => {
-  const [data, setData] = useState(null);
+    const [data, setData] = useState(null);
 
-  const fetchData = () => {
-    fetch('https://api.example.com/data')
-      .then(response => response.json())
-      .then(data => setData(data))
-      .catch(error => console.error(error));
-  };
+    const fetchData = () => {
+        fetch('https://api.example.com/data')
+            .then(response => response.json())
+            .then(data => setData(data))
+            .catch(error => console.error(error));
+    };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+    useEffect(() => {
+        fetchData();
+    }, []);
 
-  const handleFetchData = () => {
-    fetchData();
-  };
-  
-  /*if (!LoggedIn)
-  {
+    const handleFetchData = () => {
+        fetchData();
+    };
     return (
-      <p>Not logged in.</p>
-    )
-  }
-  */
-  return (
-    <div>
-      {data === null ? (
-        <p>You have no active reservations.</p>
-      ) : (
-        <div>
-          <ul>
-            {data.map(item => (
-              <li key={item.id}>{item.text}</li>
-            ))}
-          </ul>
-        </div>
-      )}
-      <button onClick={handleFetchData}>New IP-address</button>
-    </div>
-  );
+        <Container>
+            <UserProfile userData={data} />
+            <IPReservation/>
+            <IPReservedTable ipData={data} />
+        </Container>
+    );
 }
 
 
