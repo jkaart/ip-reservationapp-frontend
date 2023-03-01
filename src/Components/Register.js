7f10d7ad20cf85174506ec79c69e06ace9cb492f
pvehniainen <pyry.vehniainen@gmail.com>
2023-03-01 13:48:02 +0200

    register form styling

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -26,22 +26,26 @@ const Register = () => {
                     <Form.Control type="text" placeholder="Last Name" />
                 </Form.Group>
             </Row>*/}
-            <Form.Group>
-                <Form.Group>
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="user@esedulainen.fi" />
-                    <Form.Text className="text-muted">
-                        Enter your Esedulainen-email
-                    </Form.Text>
-                </Form.Group>
-                <Form.Label>Password</Form.Label>
-                <PasswordField className="mb-2" id="pass" onChangeProp={setNewPass} />
-                <Form.Label>Group</Form.Label>
-                <Form.Control type="text" placeholder="Group 1" />
-            </Form.Group>
-            <Button variant="primary" type="submit" className="mt-3">
-                Register
-            </Button>
+            <Row>
+                <Col md="5">
+                    <Form.Group>
+                        <Form.Group>
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="user@esedulainen.fi" />
+                            <Form.Text className="text-muted">
+                                Enter your Esedulainen-email
+                            </Form.Text>
+                        </Form.Group>
+                        <Form.Label>Password</Form.Label>
+                        <PasswordField className="mb-2" id="pass" onChangeProp={setNewPass} />
+                        <Form.Label>Group</Form.Label>
+                        <Form.Control type="text" placeholder="Group 1" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit" className="mt-3">
+                        Register
+                    </Button>
+                </Col>
+            </Row>
         </Form>
     );
 };
