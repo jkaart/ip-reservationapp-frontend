68d1938b885f26478a268a39ad9e6c95c07bbba8
temp-storage <pyry.vehniainen@gmail.com>
2023-03-06 12:44:27 +0200

    IconContext added

--- src/Components/IPReservedTable.js
+++ src/Components/IPReservedTable.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Row, Col, Button, Container } from "react-bootstrap";
 import { IPTableHeader } from "./IPTableHeader";
 import { FiEdit, FiPlusSquare, FiSquare, FiCheckSquare } from "react-icons/fi";
+import { IconContext } from "react-icons";
 
 function IPReservedTable(data) {
     const [check, checked] = useState(false);
@@ -16,22 +17,24 @@ function IPReservedTable(data) {
                 ) : (
                     <>
                         <IPTableHeader></IPTableHeader>
-                        <Row className="border rounded m-0 p-2">
-                            <Col sm md='2'>
-                                <span className="align-middle">10.36.64.20</span>
-                            </Col>
-                            <Col sm md='3'>
-                                <span className="align-middle">28.02.2023 - 7 days</span>
-                                <Button variant="" className="p-0 float-end"> <FiPlusSquare size={30} /> </Button>
-                            </Col>
-                            <Col sm md='6'>
-                                <span className="align-middle">Server</span>
-                                <Button variant="" className="p-0 float-end"> <FiEdit size={30} /> </Button>
-                            </Col>
-                            <Col sm md='1' className=''>
-                                <Button variant="" className="p-0 float-end" onClick={handleToggleCheckbox}> {check ? <FiCheckSquare size={30} /> : <FiSquare size={30} />}  </Button>
-                            </Col>
-                        </Row>
+                        <IconContext.Provider value={{ size: "30px" }}>
+                            <Row className="border rounded m-0 p-2">
+                                <Col sm md='2'>
+                                    <span className="align-middle">10.36.64.20</span>
+                                </Col>
+                                <Col sm md='3'>
+                                    <span className="align-middle">28.02.2023 - 7 days</span>
+                                    <Button variant="" className="p-0 float-end"> <FiPlusSquare /> </Button>
+                                </Col>
+                                <Col sm md='6'>
+                                    <span className="align-middle">Server</span>
+                                    <Button variant="" className="p-0 float-end"> <FiEdit /> </Button>
+                                </Col>
+                                <Col sm md='1' className=''>
+                                    <Button variant="" className="p-0 float-end" onClick={handleToggleCheckbox}> {check ? <FiCheckSquare /> : <FiSquare />}  </Button>
+                                </Col>
+                            </Row>
+                        </IconContext.Provider>
 
 
                         {/*<tbody>
