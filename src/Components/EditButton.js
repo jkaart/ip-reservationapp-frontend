87de8ed598ded9fdbf5aaab2ea23f60d012dc7ab
joonas-ris <joonas.rissanen@outlook.com>
2023-03-11 13:58:48 +0200

    EditButton styling

--- src/Components/EditButton.js
+++ src/Components/EditButton.js
@@ -1,34 +1,39 @@
 import React, { useState } from "react";
-import { Button } from "react-bootstrap";
-import { FiEdit } from "react-icons/fi";
+import { Button, FormControl } from "react-bootstrap";
+import { FiCheck, FiEdit, FiX } from "react-icons/fi";
 
-function EditButton() {
-  const [editing, setEditing] = useState(false);
-  const [text, setText] = useState("undefined");
+function EditButton(props) {
+    const {description} = props;
+    const [editing, setEditing] = useState(false);
+    const [text, setText] = useState(description);
 
-  const handleButtonClick = () => {
-    setEditing(!editing);
-  };
+    const handleButtonClick = () => {
+        setEditing(!editing);
+    };
 
-  const handleInputChange = (event) => {
-    setText(event.target.value);
-  };
+    const handleInputChange = (event) => {
+        setText(event.target.value);
+    };
 
-  const handleInputBlur = () => {
-    setEditing(false);
-  };
+    const handleInputBlur = () => {
+        setEditing(false);
+    };
 
-  return (
-    <div>
-      {editing ? (
-        <input type="text" value={text} onChange={handleInputChange} onBlur={handleInputBlur} />
-      ) : (
-        <span className="align-middle">{text}</span>
-      )}
-      <Button variant="" className="p-0 float-end" onClick={handleButtonClick}> <FiEdit /> </Button>
-    </div>
-    
-  );
+    return (
+        <>
+            {editing ? (
+                <div className="d-flex">
+                    <FormControl className="" type="text" value={text} placeholder="Description of usage" onChange={handleInputChange} onBlur={handleInputBlur} />
+                    &nbsp;&nbsp;
+                    <Button variant="" className="p-0" onClick={handleButtonClick}> <FiCheck /> </Button>
+                    <Button variant="" className="p-0" onClick={handleButtonClick}> <FiX /> </Button>
+                </div>
+            ) : (
+                <span className="align-middle">{text} <Button variant="" className="p-0 float-end" onClick={handleButtonClick}> <FiEdit /> </Button></span>
+            )}
+
+        </>
+    );
 }
 
 export default EditButton;
