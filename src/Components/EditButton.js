a85c7ece3d125e757228c744b8e3bb163b110af3
joonas-ris <joonas.rissanen@outlook.com>
2023-03-13 13:29:25 +0200

    some ui refinement

--- src/Components/EditButton.js
+++ src/Components/EditButton.js
@@ -3,33 +3,43 @@ import { Button, FormControl } from "react-bootstrap";
 import { FiCheck, FiEdit, FiX } from "react-icons/fi";
 
 function EditButton(props) {
-    const {description} = props;
+    const {description, updateRowDescription} = props;
     const [editing, setEditing] = useState(false);
-    const [text, setText] = useState(description);
+    const [text, setText] = useState({text: description, oldText: description});
 
-    const handleButtonClick = () => {
-        setEditing(!editing);
+    const handleConfirmClick = () => {
+        updateRowDescription(text.text);
+        setText({oldText:text.text, text:text.text});
+        setEditing(false);
+    };
+    const handleCancelClick = () => {
+        setText({text: text.oldText, oldText: text.oldText});
+        setEditing(false);
+    };
+    const handleEditClick = () => {
+        setEditing(true);
     };
 
     const handleInputChange = (event) => {
-        setText(event.target.value);
+        setText({text:event.target.value, oldText:text.oldText});
     };
 
     const handleInputBlur = () => {
         setEditing(false);
+        setText({text: text.oldText, oldText: text.oldText});
     };
 
     return (
         <>
             {editing ? (
                 <div className="d-flex">
-                    <FormControl className="" type="text" value={text} placeholder="Description of usage" onChange={handleInputChange} onBlur={handleInputBlur} />
+                    <FormControl className="" type="text" value={text.text} placeholder="Description of usage" onChange={handleInputChange} onBlur={handleInputBlur} />
                     &nbsp;&nbsp;
-                    <Button variant="" className="p-0" onClick={handleButtonClick}> <FiCheck /> </Button>
-                    <Button variant="" className="p-0" onClick={handleButtonClick}> <FiX /> </Button>
+                    <Button variant="success" className="p-1" onClick={handleConfirmClick}> <FiCheck /> </Button>
+                    <Button variant="danger" className="p-1" onClick={handleCancelClick}> <FiX /> </Button>
                 </div>
             ) : (
-                <span className="align-middle">{text} <Button variant="" className="p-0 float-end" onClick={handleButtonClick}> <FiEdit /> </Button></span>
+                <span className="align-middle pt-2">{text.text} <Button variant="" className="p-1 float-end" onClick={handleEditClick}> <FiEdit /> </Button></span>
             )}
 
         </>
