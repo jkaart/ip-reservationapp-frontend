ed480cd3a0c0038e1db1844228d8dce3c62141c2
joonas-ris <joonas.rissanen@outlook.com>
2023-03-20 08:11:00 +0200

    nav rework + improvements(?)

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,11 +1,11 @@
 import { DEBUG } from "../config";
 import React from "react";
-import { Link } from "react-router-dom";
+import { FiLogOut } from "react-icons/fi";
+import { Link, useLocation } from "react-router-dom";
 
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function Navigation(props) {
     let login = props.login;
@@ -24,6 +24,8 @@ function Navigation(props) {
         localStorage.removeItem('expires');
     };
 
+    const location = useLocation().pathname;
+
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
@@ -31,17 +33,16 @@ function Navigation(props) {
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        { DEBUG || login === 'admin' && <Nav.Link as={Link} to="/admin">Admin Panel</Nav.Link> }
+                        { DEBUG || login === 'admin' && !(location === '/admin') && <Nav.Link as={Link} to="/admin">Admin Panel</Nav.Link> }
+                        { login === 'admin' && location === '/admin' && <Nav.Link as={Link} to="/">IP Reservations</Nav.Link> }
                     </Nav>
-                    <Nav>
-                        <NavDropdown title="User" id="collapsible-nav-dropdown" align="end">
-                            { !login && <NavDropdown.Item as={Link} to="/">Log In</NavDropdown.Item>}
-                            { login && <NavDropdown.Item as={Link} to="/">IP Reservations</NavDropdown.Item> }
-                            { login && <NavDropdown.Item as={Link} to="/user">User Info</NavDropdown.Item> }
-                            <NavDropdown.Divider />
-                            { !login && <NavDropdown.Item as={Link} to="/register">Register</NavDropdown.Item>}
-                            { login && <NavDropdown.Item onClick={handleLogout}>Log Out</NavDropdown.Item> }
-                        </NavDropdown>
+                    <Nav align="end">
+                        { !login && location === '/register' && <Nav.Link as={Link} to="/">Log In</Nav.Link>}
+                        { login  && location === '/user' && <Nav.Link as={Link} to="/">IP Reservations</Nav.Link> }
+                        { login  && (location === '/' || location === '/admin') && <Nav.Link as={Link} to="/user">User Info</Nav.Link> }
+                        { login  && <Nav.Link disabled className="d-none d-lg-block">|</Nav.Link>}
+                        { !login && location === '/' && <Nav.Link as={Link} to="/register">Register</Nav.Link>}
+                        { login  && <Nav.Link onClick={handleLogout} title="Log out"> <span className="d-lg-none">Log out</span> <FiLogOut size='1.5em'></FiLogOut> </Nav.Link> }
                     </Nav>
                 </Navbar.Collapse>
             </Container>
